acf1ceb58d004c6ff6949bc623e548aeec5b3b02
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2017-07-14 03:22:32 +0500

    Version increment.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='bandcamp-player',
-    version='0.0.1',
+    version='0.1.0',
     packages=['bandcamp_player'],
     url='https://github.com/strizhechenko/bandcamp-player',
     license='MIT',
